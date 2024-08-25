fd3bcf89366cef256cad4bc6549c5f41a0c4b71f
AmrViernes <amraromoro@hotmail.com>
2024-08-25 14:56:38 +0300

    first commit

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,9 +1,6 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText } from 'ai';
 
-// ⚠️ Start by adding your API key to .env.local as OPENAI_API_KEY
-
-// Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
@@ -16,8 +13,7 @@ export async function POST(req: Request) {
             content: prompt,
         }],
         async onFinish() {
-            // implement your own logic here, e.g. for storing messages
-            // or recording token usage
+
         },
     });
 
